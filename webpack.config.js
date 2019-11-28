11970c45f588428ea311de7c8222130e63529b46
Aimekram <janina.byszewska@gmail.com>
2019-11-28 22:27:49 +0100

    Set rules for file- and html-loaders

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,18 @@ module.exports = {
                 use:['style-loader','css-loader']
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader'],
-            },
-            {
                 test:/\.html$/,
                 use:['html-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'imgs',
+                    }
+                }
             },
         ]
     }
