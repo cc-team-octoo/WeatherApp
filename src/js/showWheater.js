a71b6c41d9ca59c4ee245428a4d8e58fcba01577
Aimekram <janina.byszewska@gmail.com>
2019-12-01 22:10:05 +0100

    Delete function to get dates for 4 days and create function to get today's date

--- src/js/showWheater.js
+++ src/js/showWheater.js
@@ -1,20 +1,16 @@
 const icons = document.querySelectorAll('.smallCard__icon')
 
-const dates = [];
-
-//get dates for four next days
-function getDates() {
-    const today = new Date();
-    const nextDay = new Date(today);
-    for(let i = 1; i <= 4; i++) {
-        nextDay.setDate(today.getDate()+i);
-        const nextDate = nextDay.toISOString().slice(0, 10); //get date in format yyyy-mm-dd
-        dates.push(nextDate);
-    }
+function getToday() {
+    const now = new Date();
+    const today = now.toISOString().slice(0, 10);
+    console.log(today)
+    return today;
 }
+
+
 //store needed data in one object
 function getAllDaysData(arg) {
-    const allDaysData = []
+    const allDaysData = [];
     arg.list.map((i) => {
         allDaysData.push({
             date: i.dt_txt.split(' ')[0],
@@ -22,20 +18,17 @@ function getAllDaysData(arg) {
             tempMax: i.main.temp_max,
             icon: i.weather[0].icon
         })
-    })
-    console.log(allDaysData);
+    });
     return allDaysData;
 }
 
 function showWeather(arg){
-    getDates();
-    console.log(dates)
     console.log("-----Pogoda na najbliższe dni------") 
     console.log(arg) //całe dane już przerobione z json
     console.log(arg.city) // miasto jakie wyszukało
     console.log(arg.list) // lista "pogód" na najbliższy czas
     console.log("-----------") 
-    getAllDaysData(arg)
+    get4DaysData(arg)
 
 
     
