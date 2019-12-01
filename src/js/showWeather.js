ac053ab372640649149c17f1ba3270a0871a0c33
Aimekram <janina.byszewska@gmail.com>
2019-12-01 15:09:12 +0100

    Track showWeather file

---
+++ src/js/showWeather.js
@@ -0,0 +1,10 @@
+//calback do fetcha - tutaj trzeba będzie powstawiać elementy domu
+function showWeather(arg){
+    console.log("-----Pogoda na najbliższe dni------") 
+    console.log(arg) //całe dane już przerobione z json
+    console.log(arg.city) // miasto jakie wyszukało
+    console.log(arg.list) // lista "pogód" na najbliższy czas
+    console.log("-----------") 
+}
+
+export default showWeather
