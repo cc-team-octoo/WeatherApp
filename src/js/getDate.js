d786e6d0781f6a0979da70c5798cecd5a519076c
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-12-01 15:42:57 +0100

    Change on FV GetDate

--- src/js/getDate.js
+++ src/js/getDate.js
@@ -1,16 +1,16 @@
 function getDate() {
-    const dayName = ["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota"];
-    const monthName = ["stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca", "sierpnia",
-        "września", "października", "listopada", "grudnia"
+    const dayName = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    const monthName = ["January ", "February", "March", "April", "May", "June", "July", "August",
+        "September", "October", "November", "December"
     ];
-    var today = new Date();
-    var weekDay = today.getDay();
-    var month = today.getMonth();
-    var day = today.getDate();
-    var year = today.getFullYear();
-    var todayNameAndFourNextDay = [];
+    const today = new Date();
+    const weekDay = today.getDay();
+    const month = today.getMonth();
+    const day = today.getDate();
+    const year = today.getFullYear();
+    const todayNameAndFourNextDay = [];
     let todays = weekDay;
-    const dateDescription = `Dzisiaj jest ${dayName[weekDay]}, ${day} ${monthName[month]} ${year}`;
+    const dateDescription = `Today is ${dayName[weekDay]}, ${day} ${monthName[month]} ${year}`;
 
     for (let i = 0; i < 5; i++) {
         if (todays > 6) {
