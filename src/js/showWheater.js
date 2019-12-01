679ff7006e9fe82e297542b6fdc8b2d7d60b052a
Aimekram <janina.byszewska@gmail.com>
2019-12-01 21:25:38 +0100

    Create an object containing all needed data

--- src/js/showWheater.js
+++ src/js/showWheater.js
@@ -2,16 +2,29 @@ const icons = document.querySelectorAll('.smallCard__icon')
 
 const dates = [];
 
+//get dates for four next days
 function getDates() {
     const today = new Date();
-    console.log(today)
     const nextDay = new Date(today);
     for(let i = 1; i <= 4; i++) {
         nextDay.setDate(today.getDate()+i);
-        console.log(nextDay)
         const nextDate = nextDay.toISOString().slice(0, 10); //get date in format yyyy-mm-dd
         dates.push(nextDate);
     }
+}
+//store needed data in one object
+function getAllDaysData(arg) {
+    const allDaysData = []
+    arg.list.map((i) => {
+        allDaysData.push({
+            date: i.dt_txt.split(' ')[0],
+            time: i.dt_txt.split(' ')[1],
+            tempMax: i.main.temp_max,
+            icon: i.weather[0].icon
+        })
+    })
+    console.log(allDaysData);
+    return allDaysData;
 }
 
 function showWeather(arg){
@@ -22,8 +35,9 @@ function showWeather(arg){
     console.log(arg.city) // miasto jakie wyszukało
     console.log(arg.list) // lista "pogód" na najbliższy czas
     console.log("-----------") 
+    getAllDaysData(arg)
 
-    //dt_txt: "2019-12-02 12:00:00" wyciągnij main.temp_max
+
     
     //wyciągnij weather[0].icon i zrób url
     icons[0].innerHTML = '<img src="imgs/location.svg">'
